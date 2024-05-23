08087d95dc65fd89bc202f391a921c4bcccb3e91
Camilo <ecmartinezf@outlook.com>
2024-05-22 19:11:02 -0500

    change workitems logic

--- tasks.py
+++ tasks.py
@@ -1,21 +1,26 @@
 # from robocorp.tasks import task
 from robocorp.tasks import task
 from browser_manager.browser_manager import BrowserManager
-from RPA.Robocorp.WorkItems import WorkItems
+# from RPA.Robocorp.WorkItems import WorkItems
+from robocorp import workitems
 
 TARGET_URL = "https://www.aljazeera.com"
 
 
 @task
 def list_variables():
-    library = WorkItems()
-    library.get_input_work_item()
+    # library = WorkItems()
+    # library.get_input_work_item()
 
-    variables = library.get_work_item_variables()
-    print(len(variables.items()))
-    print(variables.items())
-    for variable, value in variables.items():
-        print("%s = %s", variable, value)
+    # variables = library.get_work_item_variables()
+    # print(len(variables.items()))
+    # print(variables.items())
+    # for variable, value in variables.items():
+    #     print("%s = %s", variable, value)
+    print("==> ", workitems.inputs)
+    for item in workitems.inputs:
+        print("Handling item!")
+        print(item)
 
 @task
 def search_and_save_results():
