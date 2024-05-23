591fad1d3e690148a81330debabbcf808eb5bd82
Camilo <ecmartinezf@outlook.com>
2024-05-22 20:24:38 -0500

    Clean code

--- tasks.py
+++ tasks.py
@@ -6,22 +6,12 @@ TARGET_URL = "https://www.aljazeera.com"
 
 
 @task
-def list_variables():
-    print("==> ", workitems.inputs)
-    for item in workitems.inputs:
-        print("Handling item!")
-        print('--> ',item.payload["search_word"])
-        print('--> ',item.payload["month_range"])
-
-@task
 def search_and_save_results():
+    print(f"Searching results for: {TARGET_URL}")
+    print(f"filter results by category/section/topic in {TARGET_URL} is not available.")
     for item in workitems.inputs:
         search_words = item.payload["search_word"]
         month_range = item.payload["month_range"]
     browser = BrowserManager()
     browser.search_news(TARGET_URL, search_words, int(month_range))
     browser.close_browsers()
-
-@task
-def test():
-    print("Hello world")
