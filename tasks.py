844646c2fa8cf505068edf881e4bb91a817fc8bc
Camilo <ecmartinezf@outlook.com>
2024-05-22 19:41:36 -0500

    workitem test bug fix 3

--- tasks.py
+++ tasks.py
@@ -1,7 +1,5 @@
-# from robocorp.tasks import task
 from robocorp.tasks import task
 from browser_manager.browser_manager import BrowserManager
-# from RPA.Robocorp.WorkItems import WorkItems
 from robocorp import workitems
 
 TARGET_URL = "https://www.aljazeera.com"
@@ -9,24 +7,19 @@ TARGET_URL = "https://www.aljazeera.com"
 
 @task
 def list_variables():
-    # library = WorkItems()
-    # library.get_input_work_item()
-
-    # variables = library.get_work_item_variables()
-    # print(len(variables.items()))
-    # print(variables.items())
-    # for variable, value in variables.items():
-    #     print("%s = %s", variable, value)
     print("==> ", workitems.inputs)
     for item in workitems.inputs:
         print("Handling item!")
         print('--> ',item.payload["search_word"])
-        print('--> ',item.payload["date_range"])
+        print('--> ',item.payload["month_range"])
 
 @task
 def search_and_save_results():
+    for item in workitems.inputs:
+        search_words = item.payload["search_word"]
+        month_range = item.payload["month_range"]
     browser = BrowserManager()
-    browser.search_news(TARGET_URL, "dollar", 1)
+    browser.search_news(TARGET_URL, search_words, int(month_range))
     browser.close_browsers()
 
 @task
